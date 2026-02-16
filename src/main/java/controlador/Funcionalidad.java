215b75a64dfaa70864feeafee66f1fa82e97dc2f
dglez24 <diegogonzalez1315@gmail.com>
2026-02-16 19:39:04 +0100

    fix: error de funcionalidad y dependencias añadidas

---
+++ src/main/java/controlador/Funcionalidad.java
@@ -0,0 +1,20 @@
+package controlador;
+
+import vista.VistaPrincipal;
+
+public class Funcionalidad {
+
+	public Funcionalidad(VistaPrincipal frame) {
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
